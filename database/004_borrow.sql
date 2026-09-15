7de4623fb3703c73f19c9156fa2e55e0641d1bf3
Shiela Pearl Kinoyog <shiela07132020@gmail.com>
2026-09-15 09:49:04 +0800

    added database\migration\004_borrow.sql

--- database/004_borrow.sql
+++ database/004_borrow.sql
@@ -29,40 +29,8 @@ WHERE br.borrow_return_date IS NULL
 
 ORDER BY borrow_date DESC;
 
--- Return a book
+-- RETURN A BOOK
 UPDATE borrow
 SET borrow_return_date = CURRENT_TIMESTAMP
 WHERE borrow_id = 1
      AND borrow_return_date IS NULL;
-
-
--- View returned books
-
-SELECT
-    br.book_id,
-
-    -- student name
-    CONCAT(
-        s.student_first_name,
-        '',
-        s.student_last_name
-    ) AS student_name,
-
-    -- Books
-    b.book_title,
-    b.book_author,
-    b.book_category,
-
-    -- Borrow date
-    br.borrow_date
-FROM borrow br
-
-INNER JOIN students s
-    ON br.student_id = s.student_id
-
-INNER JOIN books b
-    ON br.book_id =b.book_id
-
-WHERE br.borrow_return_date IS NOT NULL
-
-ORDER BY borrow_date DESC;
