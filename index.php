5e0a1cfd519632b3048bec8e64b699ed3b215789
Shiela Pearl Kinoyog <shiela07132020@gmail.com>
2026-09-17 10:22:08 +0800

    Added initial frontend for web application

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+// Database Connection
+$host = 'localhost';
+$db = 'it30a_lab_db';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host; dbname=$db; charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
+
+try{
+    $pdo = new PDO($dsn, $user, $pass, $options);
+}catch(PDOException $e){
+    die("Database connection failed" . $e->getMessage());
+}
+
+// Session
+session_start();
+
+// Determine current section
+$section = $_GET['section'] ?? 'students';
+
+// CRUD Operations
+$actions = $_GET['action'] ?? '';
+
+//----------------------------------
+//Students
+//----------------------------------
+
+//Fetch students
+if($section === 'students'){
+    $stmt = $pdo->query("
+        SELECT *
+        FROM students
+        ORDER by student_id
+        ");
+
+        $students = $stmt->fetchAll();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library System</title>
+</head>
+<body>
+    <style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+}
+th {
+    background-color: #f2f2f2;
+}
+</style>
+    <h1>Simple Library System</h1>
+    <nav>
+        <a href="index.php?section=students">Students</a>
+        <a href="index.php?section=books">Books</a>
+        <a href="index.php?section=borrow">Borrow</a>
+    </nav>
+    <hr>
+    <?php if($section=='students'): ?>
+    <h2>Students</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Course</th>
+                <th>Created At</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($students as $student): ?>
+            <tr>
+                <td>
+                    <?= htmlspecialchars($student['student_id']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_first_name']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_last_name']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_course']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_created_at']) ?>
+                </td>
+                <td>
+                    <a>Edit</a>
+                    |
+                    <a>Delete</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <?php endif; ?>
+    
+    <?php if($section=='books'): ?>
+        <h2>Books</h2>
+    <?php endif; ?>
+    
+    <?php if($section=='borrow'): ?>
+        <h2>Borrow</h2>
+    <?php endif; ?>
+
+</body>
+</html>
